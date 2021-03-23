bb9c5f8b2bcc7c617c42e1933c02c012545e30e4
carlosAZ200 <calape2@misena.edu.co>
2021-03-23 13:41:02 -0500

    Estilos

--- SIES/Models/Usuarios.cs
+++ SIES/Models/Usuarios.cs
@@ -8,10 +8,10 @@ namespace SIES.Models
     public class Usuarios
     {
         public int Id { get; set; }
-        public String Documento { get; set; }
+        public string Documento { get; set; }
         public string TipoDocumento { get; set; }
         public string Nombre { get; set; }
-        public String Celular { get; set; }
+        public string Celular { get; set; }
         public string Email { get; set; }
         public string Genero { get; set; }
         public int Aprendiz { get; set; }
